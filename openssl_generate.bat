8d8dd65933af3b1ba298079ec02bc127d99d9248
Jenne van der Meer <jennevdmeer@impulze.net>
2021-03-09 09:24:17 +0100

    Hmm 10 years seems too long for valid certs.

--- openssl_generate.bat
+++ openssl_generate.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Generating Root CA.
-openssl req -x509 -days 3650 -new -keyout root.key -out root.cer -config openssl_root.conf
+openssl req -x509 -days 356 -new -keyout root.key -out root.cer -config openssl_root.conf
 
 echo Registering Root CA
 certutil -enterprise -f -v -AddStore "Root" "./root.cer"
 
 echo Generating Local CA.
 openssl req -nodes -new -keyout localhost.key -out localhost.csr -config openssl_localhost.conf
-openssl x509 -days 3650 -req -in localhost.csr -CA root.cer -CAkey root.key -set_serial 123 -out localhost.cer -extfile openssl_localhost.conf -extensions x509_ext
+openssl x509 -days 356 -req -in localhost.csr -CA root.cer -CAkey root.key -set_serial 123 -out localhost.cer -extfile openssl_localhost.conf -extensions x509_ext
